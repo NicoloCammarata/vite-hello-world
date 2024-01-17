3a8479dbf50cf5c12185200cc5b4cdd9890ea0e9
NicoloCammarata <nicolo.camma@gmail.com>
2024-01-17 16:50:44 +0100

    es-completato

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<script >
+// importazione
+
+import appHeader from './components/appHeader.vue';
+
+export default {
+  data() {
+    return { count: 0 }
+  },
+  // dichiarazione
+  components: {
+    appHeader
+  }
+}
+
+</script>
+
+<template>
+    <div>
+        <!--utilizzo-->
+        <appHeader />
+        <body>
+            <div>
+                ciao
+            </div>
+        </body>
+ 
+
+
+
+    </div>
+  
+</template>
+
+<style scoped>
+
+</style>
